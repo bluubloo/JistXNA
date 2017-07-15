9f65e92384ad9073c68d11c215d0940b6a6452fb
Tim Kuizinas <tak23@students.waikato.ac.nz>
2017-07-15 19:38:39 +1200

    Input.Update() now publishes messages for key changes.

--- JistXNA/Input.cs
+++ JistXNA/Input.cs
@@ -11,16 +11,40 @@ namespace JistXNA
     //Keeps track of input and sends messages when input has changed
     class Input
     {
+        //Mouse position before update
         private Point mousePosition;
+
+        //Keyboard state before update
+        private KeyboardState keyboardState;
 
         public void Update()
         {
+            //Check to see if mouse position has moved
             if(mousePosition == null || !mousePosition.Equals(Mouse.GetState().Position))
             {
                 mousePosition = Mouse.GetState().Position;
                 MessageBus.Publish(new Message("Mouse Moved", mousePosition.X, mousePosition.Y));
             }
             
+            //Check to see if keyboard state has changed
+            if(keyboardState == null || !keyboardState.Equals(Keyboard.GetState()))
+            {
+                KeyboardState newKeyboardState = Keyboard.GetState();
+                //Publish a message for each key that was pressed since the last update
+                Keys[] down = newKeyboardState.GetPressedKeys().Except(keyboardState.GetPressedKeys()).ToArray();
+                for(int i = 0; i < down.Length; i++)
+                {
+                    MessageBus.Publish(new Message("Key Down", down[i]));
+                }
+
+                //Pulish a message for each key that was lifted up since the last update
+                Keys[] up = keyboardState.GetPressedKeys().Except(newKeyboardState.GetPressedKeys()).ToArray();
+                for (int i = 0; i < up.Length; i++)
+                {
+                    MessageBus.Publish(new Message("Key Up", up[i]));
+                }
+                keyboardState = newKeyboardState;
+            }
         }
     }
 }
