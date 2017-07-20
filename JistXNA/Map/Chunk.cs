01111d3dc78389e52e02da117f5be0f4accadca3
Tim <bloobluu@hotmail.com>
2017-07-20 16:21:35 +1200

    Start of map data structure created.

---
+++ JistXNA/Map/Chunk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JistXNA.Map
+{
+    class Chunk
+    {
+        //Holds what type a tile is
+        private uint[,] tileType;
+
+        public Chunk()
+        {
+            tileType = new uint[64,64];
+        }
+    }
+}
